987ad0141acb5f5b3dec6b014031f7d4daf0f613
kensvin <kensvin28@gmail.com>
2023-01-06 23:59:47 +0700

    QR Bug

--- my-app/src/components/qr/QRPage.tsx
+++ my-app/src/components/qr/QRPage.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {ReactInstance, useRef} from 'react';
+import ReactToPrint, { useReactToPrint } from 'react-to-print';
 import Header from "../Header";
 
 function QrPage() {
-    // const componentRef = useRef<HTMLImageElement>(null);
-    //
+    // const componentRef = useRef(null);
     // const handlePrint = useReactToPrint({
     //     content: () => componentRef.current,
     // });
-
+//TODO: make page responsive after printing, because replacing innerHTML removes the functionality
     function printQR() {
         const qr = document.getElementById("qr");
         let printContents = "";
@@ -27,7 +27,7 @@ function QrPage() {
                 <p className={"font-bold text-center"}>
                     Show your QR to receive payment
                 </p>
-                <div id="qr">
+                <div id="qr" className={"mx-auto justify-center"} >
                     <img id="x" src={process.env.PUBLIC_URL + "/qrcode.png"} className={"object-none object-center"}/>
                 </div>
                 <p className={"font-bold text-center"}>
@@ -35,6 +35,13 @@ function QrPage() {
                 </p>
             </div>
             <div className={"flex justify-center my-4"}>
+            {/*<ReactToPrint*/}
+            {/*    trigger={() => <button type={"button"} className={"px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"}>*/}
+            {/*        Print QR*/}
+            {/*    </button>}*/}
+            {/*    content={() => componentRef}*/}
+            {/*/>*/}
+
             <button type={"button"} onClick={printQR} className={"px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"}>
                 Print QR
             </button>
