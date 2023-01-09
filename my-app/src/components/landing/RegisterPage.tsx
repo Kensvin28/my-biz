7f490087e4e9d9a850ece0f87a2992cceb1738da
BW-BW <brianwidjaja53@gmail.com>
2023-01-09 16:58:24 +0800

    fix minor logic - backend

--- my-app/src/components/landing/RegisterPage.tsx
+++ my-app/src/components/landing/RegisterPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useRef } from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { supabaseAdmin } from '../../supabase';
 import Header from "../Header";
 import { useNavigate } from 'react-router-dom';
@@ -30,74 +30,94 @@ function RegisterPage() {
     };
 
     const handleChangeBusNum = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-      setInputBusinessNum(event.target.value);
-      console.log('businesnumber', inputBusinessNum);
+        setInputBusinessNum(event.target.value);
+        console.log('businesnumber', inputBusinessNum);
     };
-    
+
     const handleChangeContactPerson = (event: { target: { value: React.SetStateAction<string>; }; }) => {
         setinputContactPerson(event.target.value);
         console.log('compname:', inputContactPerson);
-      };
+    };
 
     const handleChangeinputAddress = (event: { target: { value: React.SetStateAction<string>; }; }) => {
         setinputAddress(event.target.value);
         console.log('compname:', inputAddress);
-      };
-      
+    };
+
     const handleChangePhoneNumber = (event: { target: { value: React.SetStateAction<string>; }; }) => {
         setinputPhoneNum(event.target.value);
-    console.log('compname:', inputPhoneNum);
+        console.log('compname:', inputPhoneNum);
     };
 
     const handleChangePassword = (event: { target: { value: React.SetStateAction<string>; }; }) => {
         setinputPassword(event.target.value);
-    console.log('compname:', inputPassword);
+        console.log('compname:', inputPassword);
     };
 
     const handleChangeConfirmPassword = (event: { target: { value: React.SetStateAction<string>; }; }) => {
         setinputConfPassword(event.target.value);
-    console.log('compname:', inputConfPassword);
+        console.log('compname:', inputConfPassword);
     };
 
     const handleChangePin = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setinputPin(event.target.value);
-    console.log('compname:', inputPin);
+        setinputPin(event.target.value);
+        console.log('compname:', inputPin);
     };
 
     const handleChangeConfirmPin = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setinputConfPin(event.target.value);
-    console.log('compname:', inputConfPin);
+        setinputConfPin(event.target.value);
+        console.log('compname:', inputConfPin);
     };
-      
-    async function checkAvailability(){
+
+    async function checkAvailabilityCompany() {
         //check if company name or number is used
         const { data, error } = await supabaseAdmin
-        .from('companies')
-        .select()
-        .eq('CompanyName', inputCompName)
+            .from('companies')
+            .select()
+            .eq('CompanyName', inputCompName)
 
-        if (data?.length!=0){
-            alert("company name used or number used")
+        if (data?.length != 0) {
+            alert("Company Name Already Used")
             console.log(data)
             return false
-        } 
-        else if (error){
+        }
+        else if (error) {
             throw error;
-        }else {
+        } else {
             console.log("all good")
             return true
         }
     }
 
-    async function register(){
+    async function checkAvailabilityNumber() {
+        //check if company name or number is used
+        const { data, error } = await supabaseAdmin
+            .from('companies')
+            .select()
+            .eq('BusinessRegNumber', inputBusinessNum)
+
+        if (data?.length != 0) {
+            alert("Business Registration Number Already Used!")
+            console.log(data)
+            return false
+        }
+        else if (error) {
+            throw error;
+        } else {
+            console.log("all good")
+            return true
+        }
+    }
+
+    async function register() {
         //check availability
-        if(await checkAvailability() == false){
+        if (await checkAvailabilityCompany() == false || await checkAvailabilityNumber() == false) {
             console.log("error")
             return;
         }
 
         //check pass and pin
-        if(!(inputPassword==inputConfPassword&&inputPin==inputConfPin)){
+        if (!(inputPassword == inputConfPassword && inputPin == inputConfPin)) {
             alert("Incorrect password and/or Pin")
             console.log("different password and/or Pin")
             return;
@@ -105,12 +125,12 @@ function RegisterPage() {
 
         //input to database
         const { data, error } = await supabaseAdmin.from("companies").insert({
-            CompanyName:inputCompName,
+            CompanyName: inputCompName,
             BusinessRegNumber: inputBusinessNum,
             CompanyAddress: inputAddress,
             ContactPerson: inputContactPerson,
-            PhoneNumber : inputPhoneNum,
-            Password: inputPassword ,
+            PhoneNumber: inputPhoneNum,
+            Password: inputPassword,
             PIN: inputPin,
         })
         console.log("inputted")
@@ -124,15 +144,15 @@ function RegisterPage() {
     return (
         // https://tailwind-elements.com/docs/standard/components/cards/3
         <div>
-            <Header/>
-        <div className="flex justify-center">
-            <div className="block p-6 rounded-lg shadow-lg bg-white w-max">
-                <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">Register</h5>
+            <Header />
+            <div className="flex justify-center">
+                <div className="block p-6 rounded-lg shadow-lg bg-white w-max">
+                    <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">Register</h5>
 
-                <input
-                    onChange={handleChangeComp}
-                    type="text"
-                    className="
+                    <input
+                        onChange={handleChangeComp}
+                        type="text"
+                        className="
         form-control
         block
         w-full
@@ -150,14 +170,14 @@ function RegisterPage() {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-                    id="company_name"
-                    placeholder="Company Name"
-                   />
+                        id="company_name"
+                        placeholder="Company Name"
+                    />
 
-                <input
-                    onChange={handleChangeBusNum}
-                    type="number"
-                    className="
+                    <input
+                        onChange={handleChangeBusNum}
+                        type="number"
+                        className="
         form-control
         block
         w-80
@@ -175,13 +195,13 @@ function RegisterPage() {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-                    id="business_registration_number"
-                    placeholder="Business Registration Number"/>
+                        id="business_registration_number"
+                        placeholder="Business Registration Number" />
 
-                <input
-                    onChange={handleChangeinputAddress}
-                    type="text"
-                    className="
+                    <input
+                        onChange={handleChangeinputAddress}
+                        type="text"
+                        className="
         form-control
         block
         w-full
@@ -199,13 +219,13 @@ function RegisterPage() {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-                    id="company_address"
-                    placeholder="Company Address"/>
+                        id="company_address"
+                        placeholder="Company Address" />
 
-                <input
-                    onChange={handleChangeContactPerson}
-                    type="text"
-                    className="
+                    <input
+                        onChange={handleChangeContactPerson}
+                        type="text"
+                        className="
         form-control
         block
         w-full
@@ -223,13 +243,13 @@ function RegisterPage() {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-                    id="contact_person"
-                    placeholder="Contact Person"/>
+                        id="contact_person"
+                        placeholder="Contact Person" />
 
-                <input
-                    onChange={handleChangePhoneNumber}
-                    type="number"
-                    className="
+                    <input
+                        onChange={handleChangePhoneNumber}
+                        type="number"
+                        className="
         form-control
         block
         w-full
@@ -247,13 +267,13 @@ function RegisterPage() {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-                    id="phone_number"
-                    placeholder="Phone Number"/>
+                        id="phone_number"
+                        placeholder="Phone Number" />
 
-                <input
-                    onChange={handleChangePassword}
-                    type="password"
-                    className="
+                    <input
+                        onChange={handleChangePassword}
+                        type="password"
+                        className="
         form-control
         block
         w-full
@@ -271,13 +291,13 @@ function RegisterPage() {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-                    id="password"
-                    placeholder="Create Password"/>
+                        id="password"
+                        placeholder="Create Password" />
 
-                <input
-                    onChange={handleChangeConfirmPassword}
-                    type="password"
-                    className="
+                    <input
+                        onChange={handleChangeConfirmPassword}
+                        type="password"
+                        className="
         form-control
         block
         w-full
@@ -295,13 +315,13 @@ function RegisterPage() {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-                    id="confirm_password"
-                    placeholder="Confirm Password"/>
+                        id="confirm_password"
+                        placeholder="Confirm Password" />
 
-                <input
-                    onChange={handleChangePin}
-                    type="tel"
-                    className="
+                    <input
+                        onChange={handleChangePin}
+                        type="tel"
+                        className="
         form-control
         block
         w-full
@@ -319,13 +339,13 @@ function RegisterPage() {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-                    id="pin"
-                    placeholder="Create PIN"/>
-                
-                <input
-                    onChange={handleChangeConfirmPin}
-                    type="tel"
-                    className="
+                        id="pin"
+                        placeholder="Create PIN" />
+
+                    <input
+                        onChange={handleChangeConfirmPin}
+                        type="tel"
+                        className="
         form-control
         block
         w-full
@@ -343,19 +363,19 @@ function RegisterPage() {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-                    id="confirm_pin"
-                    placeholder="Confirm PIN"/>
+                        id="confirm_pin"
+                        placeholder="Confirm PIN" />
 
-                <button type="button"
+                    <button type="button"
                         onClick={register} //function yg nge call resghiter
                         className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Register
-                </button>
+                    </button>
+                </div>
+            </div>
+            <div className="flex justify-center mt-2">
+                <Link to="/" className={"text-blue-600"}>Login</Link>
             </div>
         </div>
-    <div className="flex justify-center mt-2">
-        <Link to="/" className={"text-blue-600"}>Login</Link>
-    </div>
-    </div>
     );
 }
 
