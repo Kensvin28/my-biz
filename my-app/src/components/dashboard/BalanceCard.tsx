6b9e250831108a0df3265b1a2e1cf5359805745a
kensvin <kensvin28@gmail.com>
2023-01-10 15:05:34 +0700

    Add balance and clean up dashboard

---
+++ my-app/src/components/dashboard/BalanceCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function BalanceCard() {
+    return (
+        <div>
+            <div className="block p-6 rounded-lg shadow-lg bg-white w-1/2 max-w-md mx-auto">
+                <p>567121367160</p>
+                <p className="font-bold">Balance</p>
+                <p>RM10,000</p>
+            </div>
+        </div>
+    );
+}
+
+export default BalanceCard;
--- my-app/src/components/transaction/TransactionPage.tsx
+++ my-app/src/components/transaction/TransactionPage.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from "../Header";
+import BalanceCard from "../dashboard/BalanceCard";
 
 function TransactionPage() {
     return (
         <div>
             <Header></Header>
-            <p>Transaction</p>
+            <BalanceCard/>
+           <div className="flex flex-row justify-center">
+               <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+
+               </div>
+               <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm"></div>
+           </div>
         </div>
     );
 }
